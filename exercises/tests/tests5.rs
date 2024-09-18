042b7a09c7da47d3540068e5338c1340dcc97dd1
luvroc <139564189+luvroc@users.noreply.github.com>
2024-09-18 12:49:17 +0000

    commit3

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -22,7 +22,6 @@
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 /// # Safety
 ///
@@ -32,7 +31,9 @@ unsafe fn modify_by_address(address: usize) {
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
     unsafe {
-        todo!("Your code goes here")
+        // SAFETY: The address is guaranteed to be valid and contains
+        (address  as *mut u32 ).write_volatile(0xAABBCCDD);
+        
     }
 }
 
